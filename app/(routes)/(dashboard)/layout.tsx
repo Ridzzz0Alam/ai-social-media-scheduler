0023e4a4a2276f31dae9abb4365a42de4247dabb
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-05-21 00:29:40 +0200

    added dashboard settings

--- app/(routes)/(dashboard)/layout.tsx
+++ app/(routes)/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { Sidebar } from 'lucide-react';
 import React from 'react'
+import AppSidebar from './_common/app-sidebar';
 
 export default function DashboardLayout({
   children,
@@ -9,11 +10,12 @@ export default function DashboardLayout({
 }>) {
   return (
     <SidebarProvider>
+        <AppSidebar />
         <SidebarInset className="bg-sidebar! border-none"
-        >
-            <div>
-
-                {children}
+        > 
+            <div className="m-1 px-4 rounded-lg border border-border
+            dark:border">
+                <div className="py-2 px-3">{children}</div>
             </div>
         </SidebarInset>
     </SidebarProvider>
