f56a737b3d664c41965fcca11c3f5472171a68c3
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-05-24 14:53:45 +0200

    fixed the settings issue

--- app/(routes)/(dashboard)/_common/app-sidebar.tsx
+++ app/(routes)/(dashboard)/_common/app-sidebar.tsx
@@ -7,7 +7,6 @@ import { cn } from '@/lib/utils';
 import { Calendar, CreditCard, Lightbulb, Plus, Settings } from 'lucide-react';
 import Link from 'next/link';
 
-
 const mainNav = [
   { name: "Ideas", href: "/ideas", icon: Lightbulb },
   { name: "Schedule", href: "/schedule", icon: Calendar },
@@ -15,50 +14,51 @@ const mainNav = [
   { name: "Settings", href: "/settings", icon: Settings },
 ];
 
-
 const AppSidebar = () => {
   const pathname = usePathname();
-  const {state} = useSidebar();
+  const { state } = useSidebar();
   const isCollapsed = state === "collapsed";
-  
+
   return (
     <Sidebar collapsible="icon">
-      <SidebarHeader className={cn("p-4", isCollapsed && "p-2")}></SidebarHeader>
-      <div className="flex items-center justify-between">
-        <Logo hideName={isCollapsed}/>
-        <SidebarTrigger className="hidden md:flex -mx-8 mb-0"/>
-      </div>
-      <Button className="mt-4 w-full"
-      size={isCollapsed ? "icon": "lg"}
-      >
-        <Plus className="size-4"/>
-        {!isCollapsed && <span>New Post</span>}
+      <SidebarHeader className={cn("p-4", isCollapsed && "p-2")}>
+        <div className="flex items-center justify-between">
+          <Logo hideName={isCollapsed} />
+          <SidebarTrigger className="hidden md:flex -mx-8 mb-0" />
+        </div>
+        <Button
+          className="mt-4 w-full"
+          size={isCollapsed ? "icon" : "lg"}
+        >
+          <Plus className="size-4" />
+          {!isCollapsed && <span>New Post</span>}
         </Button>
-        <SidebarHeader>
-          <SidebarContent className={cn(!isCollapsed && "px-2")}>
-            <SidebarGroup>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  {mainNav.map((item) =>(
-                    <SidebarMenuItem key={item.name}>
-                      <SidebarMenuButton asChild
-                      isActive={pathname === item.href}
-                      tooltip={item.name}
-                      >
-                        <Link href={item.href}>
-                          <item.icon className="size-4"/>
-                          <span className="text-[14.5px]">{item.name}</span>
-                        </Link>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </SidebarGroup>
-          </SidebarContent>
-        </SidebarHeader>
-    </Sidebar>
-  )
-}
+      </SidebarHeader>
 
-export default AppSidebar
+      <SidebarContent className={cn(!isCollapsed && "px-2")}>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {mainNav.map((item) => (
+                <SidebarMenuItem key={item.name}>
+                  <SidebarMenuButton
+                    asChild
+                    isActive={pathname === item.href}
+                    tooltip={item.name}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-4" />
+                      <span className="text-[14.5px]">{item.name}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
