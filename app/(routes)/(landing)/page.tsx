84b8762165be0ec354bf7cc2787aa52ad5f65548
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-05-31 12:17:59 +0200

    added the authentication section

--- app/(routes)/(landing)/page.tsx
+++ app/(routes)/(landing)/page.tsx
@@ -2,14 +2,16 @@
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useAuth } from "@clerk/nextjs";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Home() {
   const { isSignedIn } = useAuth();
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <header className="sticky top-0 z-20 border-b  border-b border-border/60 bg-background/90 backdrop-b">
+      <header className="sticky top-0 z-20 border-b border-border/60 bg-background/90">
+
+      <div className="mx-auto max-w-7xl flex items-center justify-between px-6 py-4">
         <Logo />
         <nav></nav>
  
@@ -26,11 +28,22 @@ export default function Home() {
               </Button>
             </>
           ) : (
-            <Button asChild>
-              <Link href="/dashboard">Dashboard</Link>
-            </Button>
+            <><Button asChild
+                variant="default"
+                className="rounded-full px-5"
+              >
+                <Link href="/dashboard">Open Dashboard</Link>
+              </Button><UserButton 
+              appearance={{
+                elements: {
+                  avatarBox: "h-9 w-9"
+                }
+              }}
+              /></>
           )}
         </div>
+          
+      </div>
       </header>
     </div>
   );
