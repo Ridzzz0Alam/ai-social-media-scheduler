b785300d481d9952ea47dd00c242d2a1d7d0232c
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-06-02 13:17:42 +0200

    coded settinfs page with a tabbed navigation UI

--- app/(routes)/(dashboard)/settings/page.tsx
+++ app/(routes)/(dashboard)/settings/page.tsx
@@ -1,5 +1,5 @@
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { User } from 'lucide-react'
+import { Layers, Palette, User } from 'lucide-react'
 import React from 'react'
 
 const SettingsPage = () => {
@@ -15,14 +15,19 @@ const SettingsPage = () => {
             <div className='mb-6 w-full border-b'>
               <TabsList variant="line" className="w-fit space-x-4 group-data-horizontal/tabs:h-12"
               >
-                <TabsTrigger value="channels">
+                <TabsTrigger value="profile" className="flex items-center gap-2">
                   <User className='size-4'/>
                   Profile</TabsTrigger>
-                <TabsTrigger value="profile">Channels</TabsTrigger>
-                <TabsTrigger value="settings">Appearance</TabsTrigger>
-
+                <TabsTrigger value="channels" className="flex items-center gap-2">
+                  <Layers className='size-4'/>
+                  Channels</TabsTrigger>
+                <TabsTrigger value="appearance" className="flex items-center gap-2">
+                  <Palette className='size-4'/>
+                  Appearance</TabsTrigger>
               </TabsList>
             </div>
+
+
           </Tabs>
         </div>
       </div>
